321d617731ab6df01fd804d047271f1f85a002b0
duycuonggg <thivip201197@gmail.com>
2025-07-04 20:45:53 +0700

    deploy velcel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // không cần local host nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
-  'https://trello-web-tawny.vercel.app/'
+  'https://trello-web-tawny.vercel.app'
 ]
 
 export const BOARD_TYPES = {
